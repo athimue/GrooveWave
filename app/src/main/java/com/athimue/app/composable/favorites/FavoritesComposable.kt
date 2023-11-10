2dfdb0f516c91f41e6f28ef14e8540549487b58c
athimue <84283744+athimue@users.noreply.github.com>
2023-11-10 16:47:16 +0100

    impr(favorite): add explanation text if favorite is empty

--- app/src/main/java/com/athimue/app/composable/favorites/FavoritesComposable.kt
+++ app/src/main/java/com/athimue/app/composable/favorites/FavoritesComposable.kt
@@ -2,19 +2,35 @@ package com.athimue.app.composable.favorites
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -27,7 +43,7 @@ internal fun FavoritesComposable(viewModel: FavoritesViewModel = hiltViewModel()
     val uiState by viewModel.uiState.collectAsState()
     val context = LocalContext.current
     val titles = listOf("Tracks", "Artists", "Albums")
-    var tabSelected by remember { mutableStateOf(0) }
+    var tabSelected by remember { mutableIntStateOf(0) }
     Column {
         TitleText(
             title = "Favorites"
@@ -58,16 +74,28 @@ private fun FavoriteLazyColumn(
     uiState: FavoritesUiState,
     removeFavorite: (Long) -> Unit
 ) {
+    val items = when (tabSelected) {
+        0 -> uiState.favoriteTracks
+        1 -> uiState.favoriteArtists
+        2 -> uiState.favoriteAlbums
+        else -> uiState.favoriteTracks
+    }
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
         items(
-            items = when (tabSelected) {
-                0 -> uiState.favoriteTracks
-                1 -> uiState.favoriteArtists
-                2 -> uiState.favoriteAlbums
-                else -> uiState.favoriteTracks
-            }
+            items = items
         ) {
             FavoriteRowItem(it, removeFavorite)
+        }
+        if (items.isEmpty()) {
+            item {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp),
+                    textAlign = TextAlign.Center,
+                    text = "No favorite"
+                )
+            }
         }
     }
 }
